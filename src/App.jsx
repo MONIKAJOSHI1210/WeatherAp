c765c1b1fc31426c0fa352749650212aaabaf563
MONIKAJOSHI1210 <monikajoshi1210@gmail.com>
2024-07-05 14:17:45 +0530

    weather

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Weather from './cmponents/Weather'
+
+
+const App=()=>{
+  return (
+    <div className='app'>
+      <Weather/>
+      
+
+    </div>
+  )
+}
+
+export default App
